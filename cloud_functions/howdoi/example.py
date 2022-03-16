c547b3ca7b20afedc5260170a4ee67a81c4fe25b
parvister <parham@tura.io>
2022-03-16 15:09:41 -0700

    added an empty main.py

--- cloud_functions/howdoi/example.py
+++ cloud_functions/howdoi/example.py
@@ -31,35 +31,20 @@ def howdoi_function(request: Request):
 
     send a REST request with your programming question as the GET URL param
     """
-    # first try to get the question from the URL params
-    question = request.args.get('question', None)
-    # if question is not in URL params then try other options
-    if question is None:
-        content_type = request.headers['content-type']
-        if content_type == 'application/json':
-            # part of json request body
-            request_json = request.get_json(silent=True)
-            if request_json and 'question' in request_json:
-                question = request_json['question']
-            else:
-                raise ValueError("JSON is invalid, or missing a 'question' tag")
-        elif content_type == 'text/plain':
-            # as plain text in request data
-            name = request.data
-        elif content_type == 'application/x-www-form-urlencoded':
-            # as a form parameter
-            name = request.form.get('question')
-        else:
-            # couldn't find the question anywhere! return http error response
-            return "Unknown content type: {}".format(content_type), 400, { 'content-type': 'text/plain'}
     
+    question = request.args.get('question', None)
+    if question is None:
+        answer = "You forgot to provide a question!"
+    else:
+        # use the howdoi lib for an answer
+        answer = howdoi.howdoi(question)
+    
+    # send back response
     logger.info(f"new question: '{question}'")
-    # send back the answer to the question
-    answer = howdoi.howdoi(question)
-    # response rest header options
     response_headers = {
         'content-type': 'text/plain',
         'server-time': str(datetime.now()),
     }
     # the return is a regular flask return tuple: (data, http resp code, headers)
     return answer, 200, response_headers
+
